ae42d18c2bf8ede9d222779f5a36a3d8d1db2e65
mateolopezz <mateolopezlorenz@gmail.com>
2026-04-02 20:54:40 +0200

    Initial Spring Boot structure

---
+++ lumina-hotel/src/main/java/com/luminahotel/LuminaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.luminahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminaHotelApplication.class, args);
+	}
+
+}
