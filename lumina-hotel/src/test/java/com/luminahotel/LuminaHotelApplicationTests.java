582dcf19f7d71572f3baba5e0a0bc53d6489de78
mateolopezlorenzz <mateolopezlorenz@gmail.com>
2026-04-02 21:00:06 +0200

    Initial Spring Boot structure

---
+++ lumina-hotel/src/test/java/com/luminahotel/LuminaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
